eef5b8484ab091044e883823e5704f758860b914
Attila Horvath <sweeptic@gmail.com>
2025-03-31 14:06:35 +0200

    008 Improving Signup With Unique Email Addresses

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -20,11 +20,22 @@ async function handler(req, res) {
 
   const db = client.db();
 
+  const existingUser = await db.collection('users').findOne({ email: email });
+
+  if (existingUser) {
+    // console.log('existingUser', existingUser);
+
+    res.status(422).json({ message: 'User already exists' });
+    client.close();
+    return;
+  }
+
   const hashedPassword = await hashPassword(password);
 
   const result = await db.collection('users').insertOne({ email: email, password: hashedPassword });
 
   res.status(201).json({ message: 'Created user!' });
+  client.close();
 }
 
 export default handler;
