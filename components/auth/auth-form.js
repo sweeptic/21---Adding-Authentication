bf47b900040a9fda01b3814a5b9a34879aa53c6a
Attila Horvath <sweeptic@gmail.com>
2025-03-31 13:16:31 +0200

    006 Adding A User Signup API Route

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+
 import classes from './auth-form.module.css';
 
 function AuthForm() {
@@ -13,20 +14,16 @@ function AuthForm() {
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
-          <button
-            type='button'
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
+          <button type="button" className={classes.toggle} onClick={switchAuthModeHandler}>
             {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
