c912cd484ace33ad63ab20ff44e2f06ba294ed6d
Attila Horvath <sweeptic@gmail.com>
2025-04-01 08:33:15 +0200

    014 Adding Server-Side Page Guards (And When To Use Which Approach)

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,20 +1,18 @@
-import { useSession } from 'next-auth/react';
-
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
-  const { data: session, status } = useSession();
+  //   const { data: session, status } = useSession();
 
-  if (status === 'loading') {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  //   if (status === 'loading') {
+  //     return <p className={classes.profile}>Loading...</p>;
+  //   }
 
   // Redirect away if NOT auth
 
-  if (!session) {
-    window.location.href = '/auth';
-  }
+  //   if (!session) {
+  //     window.location.href = '/auth';
+  //   }
 
   return (
     <section className={classes.profile}>
--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,29 @@
+import { getSession } from 'next-auth/react';
+
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps(context) {
+  //   console.log('context req', context.req);
+  const session = await getSession({ req: context.req });
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
 }
 
 export default ProfilePage;
