a31e3b16f8a66ef97d3eaf399ce89a1ece0d825a
Attila Horvath <sweeptic@gmail.com>
2025-04-01 08:12:52 +0200

    013 Adding Client-Side Page Guards (Route Protection)

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,8 +1,20 @@
+import { useSession } from 'next-auth/react';
+
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
 
 function UserProfile() {
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <p className={classes.profile}>Loading...</p>;
+  }
+
   // Redirect away if NOT auth
+
+  if (!session) {
+    window.location.href = '/auth';
+  }
 
   return (
     <section className={classes.profile}>
